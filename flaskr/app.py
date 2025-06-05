2bb10bd95e32e781983d06c61bb48e2500cb3f06
julianheredia <julianheredia006@gmail.com>
2025-06-05 16:54:36 -0500

    Actualización: variables de entorno y configuración para Render

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,10 @@
 from flaskr import create_app  
+import os
 
 # Crear la aplicación
 app = create_app()
 
 # Inicializar la app
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
